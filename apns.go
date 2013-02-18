e01db9a399462a52fe979a243a561f1e8a12c524
Pranav Raja <rickdangerous1@gmail.com>
2013-02-18 23:14:03 +1100

    go fmt

--- apns.go
+++ apns.go
@@ -1,135 +1,134 @@
 package apns
 
 import (
-    "net"
-    "time"
-    "crypto/tls"
-    "bytes"
-    "encoding/binary"
-    "encoding/hex"
-    "encoding/json"
+	"bytes"
+	"crypto/tls"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"net"
+	"time"
 )
 
 type Notification struct {
-    RequestType uint8
-    Identifier uint32
-    Expiry uint32
-    TokenLength uint16
-    Token [32]byte
+	RequestType uint8
+	Identifier  uint32
+	Expiry      uint32
+	TokenLength uint16
+	Token       [32]byte
 }
 
 type NotificationAndPayload struct {
-    Notification Notification
-    Payload string
+	Notification Notification
+	Payload      string
 }
 
 type NotificationFailure struct {
-    FailureType uint8
-    Status uint8
-    Identifier uint32
+	FailureType uint8
+	Status      uint8
+	Identifier  uint32
 }
 
 func DeviceTokenAsBinary(token string) ([32]byte, error) {
-    decoded, err := hex.DecodeString(token)
-    b := [32]byte{}
-    copy(b[:], decoded)
-    return b, err
+	decoded, err := hex.DecodeString(token)
+	b := [32]byte{}
+	copy(b[:], decoded)
+	return b, err
 }
 
 func MakeNotification(identifier int, token string, payload string) NotificationAndPayload {
-    binaryToken, _ := DeviceTokenAsBinary(token)
-    return NotificationAndPayload{Notification{1, uint32(identifier), 0, 32, binaryToken}, payload}
+	binaryToken, _ := DeviceTokenAsBinary(token)
+	return NotificationAndPayload{Notification{1, uint32(identifier), 0, 32, binaryToken}, payload}
 }
 
 func NotificationToBytes(n Notification, payload []byte) (*bytes.Buffer, error) {
-    buf := new(bytes.Buffer)
-    if err := binary.Write(buf, binary.BigEndian, &n); err != nil {
-        return nil, err
-    }
-    if err := binary.Write(buf, binary.BigEndian, uint16(len(payload)));
-        err != nil {
-        return nil, err
-    }
-    buf.Write(payload)
-    return buf, nil
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.BigEndian, &n); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buf, binary.BigEndian, uint16(len(payload))); err != nil {
+		return nil, err
+	}
+	buf.Write(payload)
+	return buf, nil
 }
 
-func NotificationFailureFromBytes(resp *bytes.Buffer) (NotificationFailure) {
-    var f NotificationFailure
-    binary.Read(resp, binary.BigEndian, &f)
-    return f
+func NotificationFailureFromBytes(resp *bytes.Buffer) NotificationFailure {
+	var f NotificationFailure
+	binary.Read(resp, binary.BigEndian, &f)
+	return f
 }
 
 func ApsPayload(payload string) ([]byte, error) {
-    type tree map[string] interface{}
-    jsonPayload := tree{"aps": tree{"alert": payload}}
-    return json.Marshal(jsonPayload)
+	type tree map[string]interface{}
+	jsonPayload := tree{"aps": tree{"alert": payload}}
+	return json.Marshal(jsonPayload)
 }
 
 func ResetAfter(identifier uint32, queue []NotificationAndPayload) []NotificationAndPayload {
-    for index, n := range queue {
-        if n.Notification.Identifier > identifier {
-            return queue[index:]
-        }
-    }
-    return []NotificationAndPayload{}
+	for index, n := range queue {
+		if n.Notification.Identifier > identifier {
+			return queue[index:]
+		}
+	}
+	return []NotificationAndPayload{}
 }
 
 func Connect(host string, certFile string, keyFile string) (conn *tls.Conn, err error) {
-    conf := new(tls.Config)
-    cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-    if err != nil {
-        return
-    }
-    conf.Certificates = append(conf.Certificates, cert)
-    return tls.Dial("tcp", host, conf)
+	conf := new(tls.Config)
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return
+	}
+	conf.Certificates = append(conf.Certificates, cert)
+	return tls.Dial("tcp", host, conf)
 }
 
 func Channels(conn net.Conn) (writeChannel chan NotificationAndPayload, readChannel chan NotificationFailure, err error) {
-    readChannel = make(chan NotificationFailure, 0)
-    writeChannel = make(chan NotificationAndPayload, 100)
-    go func () {
-        for {
-            conn.SetReadDeadline(time.Now().Add(2 * time.Second))
-            failure := make([]byte, 6)
-            _, err := conn.Read(failure)
-            if err != nil {
-                close(readChannel)
-                break
-            }
-            readChannel <- NotificationFailureFromBytes(bytes.NewBuffer(failure))
-        }
-    }()
-    go func () {
-        for {
-            notification := <-writeChannel
-            apsPayload, _ := ApsPayload(notification.Payload)
-            notificationBytes, _ := NotificationToBytes(notification.Notification, apsPayload)
-            conn.Write(notificationBytes.Bytes())
-        }
-    }()
-    return
+	readChannel = make(chan NotificationFailure, 0)
+	writeChannel = make(chan NotificationAndPayload, 100)
+	go func() {
+		for {
+			conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+			failure := make([]byte, 6)
+			_, err := conn.Read(failure)
+			if err != nil {
+				close(readChannel)
+				break
+			}
+			readChannel <- NotificationFailureFromBytes(bytes.NewBuffer(failure))
+		}
+	}()
+	go func() {
+		for {
+			notification := <-writeChannel
+			apsPayload, _ := ApsPayload(notification.Payload)
+			notificationBytes, _ := NotificationToBytes(notification.Notification, apsPayload)
+			conn.Write(notificationBytes.Bytes())
+		}
+	}()
+	return
 }
 
 func SendNotifications(write chan NotificationAndPayload, read chan NotificationFailure, queue []NotificationAndPayload) {
-    for _, n := range queue {
-        write <- n
-    }
-    failure := <-read
-    if failure.Identifier != 0 {
-        SendNotifications(write, read, ResetAfter(failure.Identifier, queue))
-    }
+	for _, n := range queue {
+		write <- n
+	}
+	failure := <-read
+	if failure.Identifier != 0 {
+		SendNotifications(write, read, ResetAfter(failure.Identifier, queue))
+	}
 }
 
 func ConnectAndSend(host string, certFile string, keyFile string, queue []NotificationAndPayload) (err error) {
-    conn, err := Connect(host, certFile, keyFile)
-    if err != nil {
-        return
-    }
-    write, read, err := Channels(conn)
-    if err != nil {
-        return
-    }
-    SendNotifications(write, read, queue)
-    return
+	conn, err := Connect(host, certFile, keyFile)
+	if err != nil {
+		return
+	}
+	write, read, err := Channels(conn)
+	if err != nil {
+		return
+	}
+	SendNotifications(write, read, queue)
+	return
 }
--- apns_test.go
+++ apns_test.go
@@ -1,99 +1,99 @@
 package apns
 
 import (
-    "testing"
-    "bytes"
-    "net"
-    "time"
+	"bytes"
+	"net"
+	"testing"
+	"time"
 )
 
 func BenchmarkNotificationSend(b *testing.B) {
-    queue := make([]NotificationAndPayload, b.N)
-    for i := 0; i < b.N; i++ {
-        queue[i] = MakeNotification(i, "04049bc60fc0a90ab23619c6a33e017ab6a9ea17de42b5eb008ed1f51a0eacee", "hi iphone")
-    }
-    ConnectAndSend("gateway.sandbox.push.apple.com:2195", "dev.pem", "dev.private.pem", queue)
+	queue := make([]NotificationAndPayload, b.N)
+	for i := 0; i < b.N; i++ {
+		queue[i] = MakeNotification(i, "04049bc60fc0a90ab23619c6a33e017ab6a9ea17de42b5eb008ed1f51a0eacee", "hi iphone")
+	}
+	ConnectAndSend("gateway.sandbox.push.apple.com:2195", "dev.pem", "dev.private.pem", queue)
 }
 
 func TestSerializeNotification(t *testing.T) {
-    var notification Notification
-    payload := []byte("payload")
-    notification = Notification{1, 1, 0, 32, [32]byte{'a'}}
-    b, _ := NotificationToBytes(notification, payload)
-    expected := []byte{1,0,0,0,1,0,0,0,0,0,0x20,'a',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,'p','a','y','l','o','a','d'}
-    if !bytes.Equal(b.Bytes(), expected) {
-        t.Errorf("\nActual:    %x\n Expected: %x", b.Bytes(), expected)
-    }
+	var notification Notification
+	payload := []byte("payload")
+	notification = Notification{1, 1, 0, 32, [32]byte{'a'}}
+	b, _ := NotificationToBytes(notification, payload)
+	expected := []byte{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0x20, 'a', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 'p', 'a', 'y', 'l', 'o', 'a', 'd'}
+	if !bytes.Equal(b.Bytes(), expected) {
+		t.Errorf("\nActual:    %x\n Expected: %x", b.Bytes(), expected)
+	}
 }
 
 func TestMakeNotification(t *testing.T) {
-    n := MakeNotification(1, "ae91fa", "payload")
-    expectedToken := []byte{0xAE,0x91,0xFA,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-    actualToken := n.Notification.Token
-    if !bytes.Equal(actualToken[:], expectedToken) {
-        t.Errorf("Actual: %x, Expected: %x", actualToken, expectedToken)
-    }
+	n := MakeNotification(1, "ae91fa", "payload")
+	expectedToken := []byte{0xAE, 0x91, 0xFA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	actualToken := n.Notification.Token
+	if !bytes.Equal(actualToken[:], expectedToken) {
+		t.Errorf("Actual: %x, Expected: %x", actualToken, expectedToken)
+	}
 }
 
 func TestApsPayload(t *testing.T) {
-    payload, _ := ApsPayload("message")
-    if !bytes.Equal(payload, []byte("{\"aps\":{\"alert\":\"message\"}}")) {
-        t.Errorf("Payload: %s", payload)
-    }
+	payload, _ := ApsPayload("message")
+	if !bytes.Equal(payload, []byte("{\"aps\":{\"alert\":\"message\"}}")) {
+		t.Errorf("Payload: %s", payload)
+	}
 }
 
 func TestResetAfter(t *testing.T) {
-    queue := []NotificationAndPayload{MakeNotification(1, "a", "payload"),
-        MakeNotification(2, "b", "payload2"),
-        MakeNotification(3, "b", "payload2")}
-    queue = ResetAfter(2, queue)
-    if len(queue) != 1 {
-        t.Errorf("queue has too many elements left: ", queue)
-    }
-    if queue[0].Notification.Identifier != 3 {
-        t.Errorf("first identifier != 3")
-    }
+	queue := []NotificationAndPayload{MakeNotification(1, "a", "payload"),
+		MakeNotification(2, "b", "payload2"),
+		MakeNotification(3, "b", "payload2")}
+	queue = ResetAfter(2, queue)
+	if len(queue) != 1 {
+		t.Errorf("queue has too many elements left: ", queue)
+	}
+	if queue[0].Notification.Identifier != 3 {
+		t.Errorf("first identifier != 3")
+	}
 }
 
 // Implements net.Conn that returns a canned value when read.
 // Everything else on this Conn is a no-op
 type StubConnection struct {
-    Buffer []byte
+	Buffer []byte
 }
+
 func (conn StubConnection) Read(b []byte) (int, error) {
-    copy(b, conn.Buffer)
-    return len(conn.Buffer), nil
+	copy(b, conn.Buffer)
+	return len(conn.Buffer), nil
 }
 func (conn StubConnection) Close() error {
-    return nil
+	return nil
 }
 func (conn StubConnection) RemoteAddr() net.Addr {
-    return nil
+	return nil
 }
 func (conn StubConnection) LocalAddr() net.Addr {
-    return nil
+	return nil
 }
 func (conn StubConnection) SetReadDeadline(t time.Time) error {
-    return nil
+	return nil
 }
 func (conn StubConnection) SetWriteDeadline(t time.Time) error {
-    return nil
+	return nil
 }
 func (conn StubConnection) SetDeadline(t time.Time) error {
-    return nil
+	return nil
 }
 func (conn StubConnection) Write(b []byte) (int, error) {
-    return 0, nil
+	return 0, nil
 }
 
 func TestReadsFailures(t *testing.T) {
-    _, read, _ := Channels(StubConnection{[]byte{8,4,0,0,0,2}})
-    failure := <-read
-    if failure.Status != 4 {
-        t.Errorf("couldn't read failure status")
-    }
-    if failure.Identifier != 2 {
-        t.Errorf("couldn't read failure identifier")
-    }
+	_, read, _ := Channels(StubConnection{[]byte{8, 4, 0, 0, 0, 2}})
+	failure := <-read
+	if failure.Status != 4 {
+		t.Errorf("couldn't read failure status")
+	}
+	if failure.Identifier != 2 {
+		t.Errorf("couldn't read failure identifier")
+	}
 }
-
